4946b2fefe86ce44a1a4fcfc618d3e5371f11eab
Asif Mesbah <43216053+buckbeak99@users.noreply.github.com>
2022-08-30 10:40:30 +0600

    information update
    keywords, author name change in app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,9 @@ export class AppComponent implements OnInit {
     this.titleService.setTitle(this.title);
 
     this.metaTagService.addTags([
-      { name: "keywords", content: "Web Developer, Java Developer, Full-stack Java Developer, DevOps Engineer, Android Developer, Guilherme Borges Bastos Live Resume, Guilherme Borges Bastos Resume, Guilherme Borges Bastos CV, Curriculum Guilherme Borges Bastos, GBASTOS Resumo, gbastos live resume" },
+      { name: "keywords", content: "Blockchain Developer, DevOps Enthusiasts, Web Developer" },
       { name: "robots", content: "index, follow" },
-      { name: "author", content: "Guilherme Borges Bastos" },
+      { name: "author", content: "Mohammad Mesbah Uddin Waheed" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { name: "date", content: "2020-05-12", scheme: "YYYY-MM-DD" },
       { charset: "UTF-8" }
